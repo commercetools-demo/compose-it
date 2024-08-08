1153ad4711ef378fc133488aacfcd6d7ce0eedeb
Behnam Tehrani <behnam777@gmail.com>
2024-08-08 09:27:08 -0400

    Add textField to lib

--- builder/src/components/library/index.tsx
+++ builder/src/components/library/index.tsx
@@ -3,6 +3,7 @@ import Link from '@commercetools-uikit/link';
 import Text from '@commercetools-uikit/text';
 import Card from '@commercetools-uikit/card';
 import TextInput from '@commercetools-uikit/text-input';
+import TextField from '@commercetools-uikit/text-field';
 import DataTable from '@commercetools-uikit/data-table';
 import PrimaryButton from '@commercetools-uikit/primary-button';
 import Form from './components/form';
@@ -11,13 +12,14 @@ import {
   FormDetailPage,
 } from '@commercetools-frontend/application-components';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const componentLibrary: Record<string, React.FC<any>> = {
+export const componentLibrary: Record<string, React.FC<any> | any> = {
   ...Object.keys(Text).reduce(
     (acc, key) => ({ ...acc, [`Text.${key}`]: Text[key] }),
     {}
   ),
   PrimaryButton,
   TextInput,
+  TextField,
   DataTable,
   Card,
   Link,
